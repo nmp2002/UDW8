a883432b6c86174719bd7480836f25f857aa7f97
Phu Nguyen Minh <20010978@st.phenikaa-uni.edu.vn>
2022-12-01 17:42:07 +0700

    update

---
+++ main1.js
@@ -0,0 +1,18 @@
+// console.log("it is running");
+
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navBar');
+
+if (bar){ 
+    bar.addEventListener('click', ()=>{
+        nav.classList.add('active');
+    })
+} 
+
+if (close){ 
+    close.addEventListener('click', ()=>{
+        nav.classList.remove('active');
+    })
+} 
+
